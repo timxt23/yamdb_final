83f2d264d9a983c77da2ba7ce3f5fe4188c6e8bb
timxt23 <112951950+timxt23@users.noreply.github.com>
2023-05-30 01:49:25 +0300

    19

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = '546939222'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['84.201.157.246', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['84.201.157.246', 'localhost', '127.0.0.1', 'web']
 
 # Application definition
 
